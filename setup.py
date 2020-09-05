8cce4a6e4d5cc5c80afbce8820d1256346264082
Luiz Eduardo Amaral <luizamaral306@gmail.com>
2020-09-04 21:15:00 -0300

    fix: add picamera dependency

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask"],
+    install_requires=["picamera", "flask"],
     extras_require={"test": ["pytest", "coverage"]},
     keywords=["raspberrypi", "camera", "http"],
     classifiers=[
